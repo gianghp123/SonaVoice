1ffa7724e40809f85248bc94e06870680cc76863
gianghp123 <giangthpt123@gmail.com>
2026-06-15 08:35:09 +0700

    feat: add responsive foundation (Drawer, useMediaQuery, sidebar migration)

---
+++ web/src/hooks/use-media-query.ts
@@ -0,0 +1,25 @@
+"use client"
+
+import { useSyncExternalStore } from "react"
+
+function subscribe(query: string, callback: () => void) {
+  const mql = window.matchMedia(query)
+  mql.addEventListener("change", callback)
+  return () => mql.removeEventListener("change", callback)
+}
+
+function getSnapshot(query: string) {
+  return window.matchMedia(query).matches
+}
+
+function getServerSnapshot() {
+  return false
+}
+
+export function useMediaQuery(query: string) {
+  return useSyncExternalStore(
+    (callback) => subscribe(query, callback),
+    () => getSnapshot(query),
+    getServerSnapshot
+  )
+}
